0e94b97a035366b081f7d045d7e90684a84fcce7
Trương Thành Ä?ại <thanhdaibl1572002@gmail.com>
2024-03-24 22:26:56 +0700

    Update useMediaScreen.ts

--- hooks/useMediaScreen.ts
+++ hooks/useMediaScreen.ts
@@ -1,11 +1,11 @@
-'use client'
 import { useState, useEffect } from 'react'
 
 const useMediaScreen = () => {
-    const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+    const [screenWidth, setScreenWidth] = useState(0)
     useEffect(() => {
         const handleResize = () => setScreenWidth(window.innerWidth)
         window.addEventListener('resize', handleResize)
+        handleResize()
         return () => {
             window.removeEventListener('resize', handleResize)
         }
